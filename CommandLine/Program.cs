2f199c4ae291d4ac02e56f53b83a0a3a38de98c9
Reuben Brown <reubenbrown13@gmail.com>
2024-04-05 15:53:11 -0500

    * there was a mistake when merging code. fixed it for the linux paths.

--- CommandLine/Program.cs
+++ CommandLine/Program.cs
@@ -56,10 +56,6 @@ namespace CommandLine
       if (ServerType == ServerType.Staging)
       {
         serverTypeString = "staging";
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-        {
-          serverTypeString = "staging_x64";
-        }
       }
       if (ServerType == ServerType.Playground)
       {
